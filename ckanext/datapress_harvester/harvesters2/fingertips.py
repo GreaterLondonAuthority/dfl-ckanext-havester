7d07a3cccbc91e118d92ea950d8d314fce2ad408
Oskar Przybylski <oskar.przybylski23@gmail.com>
2025-04-07 16:57:56 +0100

    update transform() for single call

--- ckanext/datapress_harvester/harvesters2/fingertips.py
+++ ckanext/datapress_harvester/harvesters2/fingertips.py
@@ -1,4 +1,3 @@
-import json
 import requests
 import logging
 from datetime import datetime
@@ -6,9 +5,6 @@ from typing import Any, Iterable
 
 from ckanext.datapress_harvester.harvesters2.lib.utils import Collector, SimpleStandard
 from ckanext.datapress_harvester.harvesters2.lib.harvesters import SimpleHarvester
-
-from ckanext.harvest.harvesters import HarvesterBase
-from ckanext.harvest.model import HarvestObject
 
 log = logging.getLogger(__name__)
 
@@ -25,40 +21,45 @@ class FingertipsCollect(Collector[dict[str, Any]]):
     def fetch(self, from_url: str) -> dict[str, Any]:
         return requests.get(from_url).json()
 
-    def transform(self, source_details: dict[str, Any],  upstream_url: str, org_name: str) -> Iterable[SimpleStandard]:
-        # find the most recent update between "uploaded" and "deleted" dates
-        src_last_updated = None
-        if source_details.get("DataChange"):
+    def transform(self, sources: dict[str, Any], upstream_url: str, org_name: str) -> Iterable[SimpleStandard]:
+        transformed_sources = []
 
-            timestamps = [datetime.strptime(source_details["DataChange"].get("LastUploadedAt", None), "%Y-%m-%dT%H:%M:%S"),
-                          datetime.strptime(source_details["DataChange"].get("LastDeletedAt", None), "%Y-%m-%dT%H:%M:%S")]
+        for source in sources:
+            source_data = sources[source]
+            # find the most recent update between "uploaded" and "deleted" dates
+            src_last_updated = None
+            if source_data.get("DataChange"):
+                timestamps = [datetime.strptime(source_data["DataChange"]["LastUploadedAt"], "%Y-%m-%dT%H:%M:%S"),
+                              datetime.strptime(source_data["DataChange"]["LastDeletedAt"], "%Y-%m-%dT%H:%M:%S")]
 
-            src_last_updated = max(
-                timestamps, default=None)
+                src_last_updated = max(
+                    timestamps, default=None)
 
-        source_descriptive = source_details.get("Descriptive")
+            source_descriptive = source_data["Descriptive"]
 
-        yield SimpleStandard(
-            package_id=f"fingertips-{source_details.get('IID')}",
-            title=source_descriptive.get("Name", None),
-            description=source_descriptive.get(
-                "Definition", None),
-            author=source_descriptive.get("DataSource", None),
-            maintainer=source_descriptive.get(
-                "srcSourceLink", None),
-            update_frequency=source_descriptive.get(
-                "Frequency", None),
-            private=False,
-            notes=source_descriptive.get(
-                "Notes", None),
-            # todo: check 'LatestChangeTimestampOverride' response field as alternative
-            data_updated_at=src_last_updated,
-            upstream_url=upstream_url,
-            org_name=org_name
-        )
+            transformed_sources.append(SimpleStandard(
+                package_id=f"fingertips-{source_data.get('IID')}",
+                title=source_descriptive.get("Name"),
+                description=source_descriptive.get(
+                    "Definition"),
+                author=source_descriptive.get("DataSource"),
+                maintainer=source_descriptive.get(
+                    "srcSourceLink"),
+                update_frequency=source_descriptive.get(
+                    "Frequency"),
+                private=False,
+                notes=source_descriptive.get(
+                    "Notes"),
+                # todo: check 'LatestChangeTimestampOverride' response field as alternative
+                data_updated_at=src_last_updated,
+                upstream_url=upstream_url,
+                org_name=org_name
+            ))
 
+        return transformed_sources
 
 # TESTING
+
 # def collector():
 #     return FingertipsCollect()
 
@@ -68,27 +69,15 @@ class FingertipsCollect(Collector[dict[str, Any]]):
 
 # print("Fetching metadata...")
 # for url in urls:
-#     package_data = collector().fetch(url)
+#     content = collector().fetch(url)
 
 # print("\nTransforming metadata...")
 # index = 1
-# for i, source in enumerate(package_data):
-#     try:
-#         transformed_source_data = collector().transform(
-#             package_data[source])
+# transformed_source_data = collector().transform(
+#     content, upstream_url="up_url", org_name="org_example")
 
-#         index = index + 1
-
-#         if i == 800:
-#             print(
-#                 f"SOURCE: \n: {json.dumps(package_data[source], indent = 2)}")
-#             print(f"\n TRANSFORMED: \n : {transformed_source_data}")
-
-#     except Exception as e:
-#         print(f"error transforming {i+1}th source id: {source}: {e}")
-#         raise
-
-# print(f"\n Transformed metadata for {index} sources")
+# print(transformed_source_data[800])
+# print(f"\n Transformed metadata for {len(transformed_source_data)} sources")
 
 # HARVESTER
 
