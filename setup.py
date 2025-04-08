f0994cfc91d9b2b1284c2207e32f091a343134fe
Oskar Przybylski <oskar.przybylski23@gmail.com>
2025-04-08 10:36:54 +0100

    implement UKPN harvester

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
         redbridge_harvester=ckanext.datapress_harvester.harvesters:RedbridgeHarvester
         soda_harvester=ckanext.datapress_harvester.harvesters:SODAHarvester
         tflunified_harvester=ckanext.datapress_harvester.harvesters2.tflunified:TflUnifiedHarvester
+        ukpn_harvester=ckanext.datapress_harvester.harvesters2.ukpn:UkpnHarvester
     """,
     # If you are changing from the default layout of your extension, you may
     # have to change the message extractors, you can read more about babel
