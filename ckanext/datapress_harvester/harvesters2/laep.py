c481db258d5664493f7d6cabdf2bbc16c16a11df
Joe <joe.saunders@london.gov.uk>
2025-11-03 16:27:53 +0000

    datetime handling on ckan python version

--- ckanext/datapress_harvester/harvesters2/laep.py
+++ ckanext/datapress_harvester/harvesters2/laep.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import dateutil.parser
 
 import requests
 from typing import Any, Iterable
@@ -7,35 +7,32 @@ from ckanext.datapress_harvester.harvesters2.lib.utils import Collector, SimpleS
 
 
 class LAEPCollect(Collector[dict[str, Any], dict[str, Any]]):
-
-    dcat_url = "https://laep-datahub-alpha-cityhall.hub.arcgis.com/api/feed/dcat-ap/2.0.1.json"
+    dcat_url = "https://laep-datahub-alpha-cityhall.hub.arcgis.com/api/feed/dcat-ap/3.0.0.json"
 
     @classmethod
     def gather(cls) -> list[dict[str, Any]]:
-
         content = requests.get(cls.dcat_url).json()
         return content["dcat:dataset"]
 
     @classmethod
     def gather_identifier(cls, source_data: dict[str, Any]) -> str:
-
         return source_data["@id"]
 
     @classmethod
-    def fetch(cls, from_url: dict[str, Any]) -> dict[str, Any]:
-
-        return from_url
+    def fetch(cls, source_data: dict[str, Any]) -> dict[str, Any]:
+        return source_data
 
     @classmethod
     def transform(cls, source_data: dict[str, Any], org_name: str) -> Iterable[SimpleStandard]:
-
         yield SimpleStandard(
             package_id=SimpleStandard.create_hashed_id(cls.gather_identifier(source_data)),
             org_name=org_name,
             upstream_url=cls.dcat_url,
             title=source_data["dct:title"],
             description=source_data["dct:description"],
-            upstream_metadata_modified=datetime.fromisoformat(source_data["dct:modified"]),
-            upstream_metadata_created=datetime.fromisoformat(source_data["dct:issued"]),
-            author=source_data["dct:publisher"]
+
+            # datetime.isoformat() works with a trailing z from python3.11 onwards
+            upstream_metadata_modified=dateutil.parser.isoparse(source_data["dct:modified"]["@value"]),
+            upstream_metadata_created=dateutil.parser.isoparse(source_data["dct:issued"]["@value"]),
+            author=source_data["dct:publisher"]["foaf:name"]
         )
