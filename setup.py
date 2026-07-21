4735b413dc389cc72d850e9c2e9ce4ca0ae3d1aa
Glannatree <anna.trichkine@london.gov.uk>
2026-07-21 13:44:08 +0100

    Replace OpenCLIMHarvester with DAFNI harvester

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         tflopen_harvester=ckanext.datapress_harvester.harvesters2.lib.harvesters:TflOpenHarvester
         laep_harvester=ckanext.datapress_harvester.harvesters2.lib.harvesters:LAEPHarvester
         instantatlas_harvester=ckanext.datapress_harvester.harvesters2.lib.harvesters:InstantAtlasHarvester
-        openclim_harvester=ckanext.datapress_harvester.harvesters2.lib.harvesters:OpenCLIMHarvester
+        openclim_harvester=ckanext.datapress_harvester.harvesters2.lib.harvesters:DAFNI
     """,
     # If you are changing from the default layout of your extension, you may
     # have to change the message extractors, you can read more about babel
